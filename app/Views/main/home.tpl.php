43de45da91d6fa01b09d5d229e9fa6374ad0828d
YoanBuscail <yoan.buscail@gmail.com>
2023-08-05 12:02:18 +0200

    ajout carroussel

--- app/Views/main/home.tpl.php
+++ app/Views/main/home.tpl.php
@@ -1,53 +1,145 @@
 <div class="intro_wrapper reveal">
     <h1 class="reveal-1">Le Labo de la Gaufre</h1>
-    <span class="subtitle reveal-2">Spécialités de gaufres salées et sucrées</span>
+    <span class="subtitle reveal-2">Fresh and Original Food</span>
 </div>
 
 <div class="general_wrapper">
-    <div class="a-propos_general_wrapper reveal">
+    <div class="general_container reveal">
         <h2 class="reveal-1">A propos de nous</h2>
-        <section class="a-propos_wrapper_container">
-            <section class="a-propos_wrapper_left">
+        <section class="wrapper_container">
+            <div class="wrapper_left">
                 <p class="reveal-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, ad. Dolor, tempora quod sunt cupiditate tempore perferendis sapiente labore quae ut, neque, asperiores impedit aliquam quos nostrum aliquid! Ducimus, earum.
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, ad. Dolor, tempora quod sunt cupiditate tempore perferendis sapiente labore quae ut, neque, asperiores impedit aliquam quos nostrum aliquid! Ducimus, earum.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, ad. Dolor, tempora quod sunt cupiditate tempore perferendis sapiente labore quae ut, neque, asperiores impedit aliquam quos nostrum aliquid! Ducimus, earum.
-                r adipisicing elit. Quae, ad. Dolor, tempora quod sunt cupiditate tempore perferendis sapiente labore quae ut, neque, asperiores impedit aliquam quos nostrum aliquid! Ducimus, earum.
                 </p>
                 <img class="waffle_picture reveal-2" src="<?= $assetsBaseUri ?>images/thai.jpg" alt="gaufre thaï" />
-            </section>
-            <section class="a-propos_wrapper_right">
+            </div>
+            <div class="wrapper_right">
                 <img class="waffle_picture reveal-3" src="<?= $assetsBaseUri ?>images/burger.jpg" alt="gaufre burger" />
                 <p class="reveal-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, ad. Dolor, tempora quod sunt cupiditate tempore perferendis sapiente labore quae ut, neque, asperiores impedit aliquam quos nostrum aliquid! Ducimus, earum.
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, ad. Dolor, tempora quod sunt cupiditate tempore perferendis sapiente labore quae ut, neque, asperiores impedit aliquam quos nostrum aliquid! Ducimus, earum.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, ad. Dolor, tempora quod sunt cupiditate tempore perferendis sapiente labore quae ut, neque, asperiores impedit aliquam quos 
                 </p>
-                
+            </div>
+        </section>
+        <div class="items reveal-4">
+            <div class="item active">
+                <img src="<?= $assetsBaseUri ?>images/burger-raclette.png">
+            </div>
+            <div class=" item next">
+                <img src="<?= $assetsBaseUri ?>images/campagnarde.png">
+            </div>
+            <div class="item">
+                <img src="<?= $assetsBaseUri ?>images/oreo.png">
+            </div>
+            <div class="item">
+                <img src="<?= $assetsBaseUri ?>images/ete.png">
+            </div>
+            <div class="item prev">
+                <img src="<?= $assetsBaseUri ?>images/fish.png">
+            </div>
+            <div class="item prev">
+                <img src="<?= $assetsBaseUri ?>images/tagada.png">
+            </div>
+            <div class="item prev">
+                <img src="<?= $assetsBaseUri ?>images/grecque.png">
+            </div>
+            <div class="item prev">
+                <img src="<?= $assetsBaseUri ?>images/halloween.png">
+            </div>
+            <div class="item prev">
+                <img src="<?= $assetsBaseUri ?>images/noel.png">
+            </div>
+            <div class="item prev">
+                <img src="<?= $assetsBaseUri ?>images/normande.png">
+            </div>
+            <div class="item prev">
+                <img src="<?= $assetsBaseUri ?>images/raclette.png">
+            </div>
+		<div class="button-container">
+			<div class="button"><i class="fas fa-angle-left"></i></div>
+			<div class="button"><i class="fas fa-angle-right"></i></div>
+		</div>
+	</div>
+    </div>
+</div>
+
+<div class="general_wrapper reveal">
+    <div class="general_container">
+        <h2 class="reveal-1">Menu</h2>
+        <section class="wrapper_container">
+            <section class="wrapper_left">
+                <h3>Les Gaufres salées</h3>
+                <ul>
+                    <li>La Lasagne
+                    Béchamel et bolognaise maisons, gratinée à l'emmental</li>
+                    <li>La Végétarienne
+                    Légumes du Marché rôtis et croquants et sa crème d'ail</li>
+                    <li>La Tarti-Gaufre
+                    Pommes de Terre, Lardons et Oignons caramélisés, 
+                    Gratinée au Reblochon</li>
+                    <li>La Thaï
+                    Gaufre Gingembre-Citron, Emincé de Poulet et Légumes Thaï</li>
+                    <li>L'Aubrac
+                    Aligot maison et Saucisse de Toulouse</li>
+                    <li>La Gaufre Burger
+                    Steak haché façon Bouchère, Bacon, Salade, Tomate, Oignons caramélisés, Crème de Cheddar maison, accompagnée de Gaufrites</li>
+                    <li>La Fish
+                    Gaufre Gingembre-Citron, Mousseline de Cabillaud la Menthe, Gratinée au Parmesan</li>
+                    <li>La Sud-Ouest
+                    Aiguillettes de Magret de Canard aux Pommes et sa Sauce au Cidre, accompagnée de Gaufrites</li>
+                    <li>La Pesto
+                    Pièce du Boucher, Copeaux de Parmesan, Tomates Séchées, Pesto Rouge, accompagnée de Gaufrites</li>
+                    <li>La Gambas
+                    Gambas et spaghetti de légumes à la sauce Thaï pimentée, sauce Curry Coco</li>
+                </ul>
+            </section>
+
+            <section class="wrapper_right">
+                <h3>Les Gaufres sucrées</h3>
+                <ul>
+                    <li>Les Gaufres à Composer
+                        <ul>
+                            <li>Nutella</li>
+                            <li>Chocolat chaud maison</li>
+                            <li>Caramel au beurre salé maison</li>
+                            <li>Confiture fraise/framboise/abricot</li>
+                            <li>Crème de Spéculoos</li>
+                            <li>Crème de marron</li>
+                            <li>Chocolat blanc maison</li>
+                            <li>Miel</li>
+                        </ul>
+                    </li>
+                    <li>Les Crèmes Maison
+                        <ul>
+                            <li>Citron meringuée : crème citron, meringue, chantilly</li>
+                            <li>Tiramisu : crème tiramisu, glace café, chantilly</li>
+                            <li>Banoffee : crème banoffe, speculoos, glace speculoos, chantilly</li>
+                        </ul>
+                    </li>
+                    <li>Les Spéciales
+                        <ul>
+                            <li>M&M's : Sauce M&M's maison, M&M's, glace vanille, chantilly</li>
+                            <li>Kinder Surprise : Chocolat blanc maison, Kinder Surprise, glace chocolat, chantilly</li>
+                            <li>Fruits rouges : Fruits rouges, glace menthe, chantilly</li>
+                        </ul>
+                    </li>
+                    <li>Les Spirituelles
+                        <ul>
+                            <li>Normande : Pommes flambées au Calvados, glace vanille, amandes grillées, chantilly</li>
+                            <li>Grand Marnier : Crème de marron, Grand Marnier, glace vanille, chantilly</li>
+                        </ul>
+                    </li>
+                    <li>Café Gourmand
+                    Mouillettes de gaufres avec 3 accompagnements au choix + glace vanille
+                    </li>
+                </ul>
             </section>
         </section>
-    </div>
-
-    <div class="menu_wrapper reveal">
-        <h2 class="reveal-1">Menu</h2>
-        <div class='left_div'>
-            <div>
-                <h3>Les Gaufres salées</h3>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt perspiciatis officia autem consequuntur sit soluta ullam doloribus optio illo perferendis eos fugit, magni officiis fuga mollitia rem qui, quod voluptate!
-                </p>
-            </div>
-        </div>
-
-        <div class='right_div'>
-            <div>
-                <h3>Les Gaufres sucrées</h3>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt perspiciatis officia autem consequuntur sit soluta ullam doloribus optio illo perferendis eos fugit, magni officiis fuga mollitia rem qui, quod voluptate!
-                </p>
-            </div>
-        </div>
-
         <a href="">Télécharger le menu</a>
     </div>
+</div>
 
-    <div class="opening-time_wrapper reveal">
+</div>
+    <div class="general_wrapper reveal">
         <h2 class="reveal-1">Horaires d'ouverture</h2>
             <ul>
                 <li>Lundi</li>
@@ -60,25 +152,25 @@
             </ul>
     </div>
 
-    <div class="access_wrapper reveal">
+    <div class="general_wrapper reveal">
         <h2 class="reveal-1">Accès</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate reprehenderit molestiae fugiat corporis repellendus! In vero repellendus voluptatum ut corrupti beatae libero debitis temporibus! Esse quaerat sed assumenda dolor! Sequi.
         </p>
     </div>
 
-    <div class="service_wrapper reveal">
+    <div class="general_wrapper reveal">
         <h2 class="reveal-1">Services</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate reprehenderit molestiae fugiat corporis repellendus! In vero repellendus voluptatum ut corrupti beatae libero debitis temporibus! Esse quaerat sed assumenda dolor! Sequi.
         </p>
     </div>
 
-    <div class="reservation_wrapper reveal">
+    <div class="general_wrapper reveal">
         <h2 class="reveal-1">Réservation</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate reprehenderit molestiae fugiat corporis repellendus! In vero repellendus voluptatum ut corrupti beatae libero debitis temporibus! Esse quaerat sed assumenda dolor! Sequi.
         </p>
     </div>
 
-    <div class="contact_wrapper reveal">
+    <div class="general_wrapper reveal">
         <h2 class="reveal-1">Contact</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate reprehenderit molestiae fugiat corporis repellendus! In vero repellendus voluptatum ut corrupti beatae libero debitis temporibus! Esse quaerat sed assumenda dolor! Sequi.
         </p>
